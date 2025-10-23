675d4aec5ee271c38a8d91826f74072590c95978
opa-ralte <nyletlaropa@gmail.com>
2025-10-23 14:15:41 +0530

    I am able to draw a line!!!

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+random = pygame.font.Font(filename="random.txt")
+
 while running:
 
     for event in pygame.event.get():
@@ -13,6 +15,9 @@ while running:
             running = False
 
     screen.fill("cornflowerblue")
+
+    # I wanna draw a line
+    pygame.draw.line(screen, (200, 0, 0), (100, 100), (500, 500), 50)
 
     pygame.display.flip()
 

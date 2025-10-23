8a6310c47c72b866532687e53abfb773d4e4a0a1
opa-ralte <nyletlaropa@gmail.com>
2025-10-23 15:45:47 +0530

    I need to learn how to use classes and objects for this purpose damnnnn...

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ ORIGINAL_RES = (1280, 720)
 screen = pygame.display.set_mode(ORIGINAL_RES)
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+font = pygame.font.Font(None, 50)
+title_surf = font.render("tilte", True, (200, 50, 225))
+title_rect = title_surf.get_frect(center=(500, 500))
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, type, name, position, direction, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.type = type
+        self.name = name
+        self.position = position
+        self.direction = direction
+        self.speed = speed
+
+    def update():
+        pass
 
 
 
@@ -19,10 +36,10 @@ while running:
 
     screen.fill("cornflowerblue")
 
-    
+    screen.blit(title_surf, title_rect)
 
     pygame.display.flip()
 
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
